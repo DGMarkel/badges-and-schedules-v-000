b02155f1666ce94bdb8025b1843faa5ce90fcc11
David Markel <davidgmarkel@gmail.com>
2018-03-11 20:01:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,7 @@ def assign_rooms(names)
 end
 
 def printer(names)
-    puts assign_rooms(names)
-    puts batch_badge_creator(names)
+    names.each do |attendee|
+      puts assign_rooms(attendee)
+    puts batch_badge_creator(attendee)
 end
